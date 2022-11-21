ee1293affc8ff33f38c45c7d1b356848a71d8bc2
GG <gerardstjosol772@gmail.com>
2022-11-22 01:45:54 +0800

    Added level select

---
+++ RegEx Edugame/Assets/Scripts/SceneHandler/CompletionChecker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CompletionChecker
+{
+    public static bool lvl1Complete;
+    public static bool lvl2Complete;
+    public static bool lvl3Complete;
+    public static bool lvl4Complete;
+    public static bool lvl5Complete;
+    public static bool lvl6Complete;
+    public static bool lvl7Complete;
+    public static bool lvl8Complete;
+    public static bool lvl9Complete;
+    public static bool lvl10Complete;
+}
